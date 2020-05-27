35e78f7b1a52dd8b3b0bd4160350e9a61cfc0b22
Carlos Villa <carlosing10@gmail.com>
2020-05-27 00:34:23 -0500

    Adding dotenv and node-config

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+const dotenv = require('dotenv');
+
+//config dotenv
+const dotenvConfig = dotenv.config();
+const config = require('config');
+
+const PORT = config.get('defaultPort');
+console.log(config, dotenvConfig);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
 });
